0e22309701565c0a5dfe80a2feb4ca0629004473
air <airmanhigh@gmail.com>
2020-07-23 15:18:12 +0800

    Smex, SmartParens

--- init.el
+++ init.el
@@ -67,4 +67,30 @@
 ;; Company
 (global-company-mode 1)
 
+;; Smex
+(require 'Smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; SmartParens
+(require 'smartparens-config)
+
 ;; ==END== PLUGIN ==END==
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (smex company hungry-delete swiper counsel smartparens js2-mode nodejs-repl exec-path-from-shell monokai-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
