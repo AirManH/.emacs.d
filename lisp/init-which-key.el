115dc5e8e1f02dfceebbb13cb311d338ee7b3aaa
air <airmanhigh@gmail.com>
2020-09-20 20:46:17 +0800

    use package: which-key

---
+++ lisp/init-which-key.el
@@ -0,0 +1,12 @@
+;;; init-which-key.el --- Shows candidate key bindings in a popup -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+
+;; (use-package which-key
+;;   :config
+;;   (which-key-mode))
+
+
+(provide 'init-which-key)
+;;; init-which-key.el ends here
