1c96192864484f732ec61cabf8128b2780d05f6c
air <airmanhigh@gmail.com>
2020-09-28 12:00:20 +0800

    fix: problem in configuring flycheck and company in cc-mode

--- lisp/init-c-gtags.el
+++ lisp/init-c-gtags.el
@@ -26,10 +26,19 @@
 (use-package company-c-headers
   :defer t)
 
-(use-package cc-mode
-  :config
-  ;; deactivate flycheck
-  (flycheck-mode 0)
+(defun air-c-gtags-disable-flycheck ()
+  "To disable flycheck mode in C/C++ mode."
+  (interactive)
+  (flycheck-mode 0))
+
+(use-package flycheck
+  :init
+  (add-hook 'c-mode-hook 'air-c-gtags-disable-flycheck)
+  (add-hook 'c++-mode-hook 'air-c-gtags-disable-flycheck))
+
+(defun air-c-gtags-set-company ()
+  "Set backends and sensitivity of company."
+  (interactive)
   ;; set company backends
   (set (make-local-variable 'company-backends)
        '((company-gtags company-c-headers company-clang company-dabbrev-code)
@@ -38,6 +47,11 @@
   (set (make-local-variable 'company-idle-delay) 0.5)
   (set (make-local-variable 'company-minimum-prefix-length) 2)
   )
+
+(use-package company
+  :init
+  (add-hook 'c-mode-hook 'air-c-gtags-set-company)
+  (add-hook 'c++-mode-hook 'air-c-gtags-set-company))
 
 
 (provide 'init-c-gtags)
