becc81f8ad1b2b5e57ab1e6a6c9835c1dd06d7fe
air <airmanhigh@gmail.com>
2020-10-18 21:55:44 +0800

    feat: confirm before auto update installed packages

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -47,6 +47,7 @@
   (setq auto-package-update-delete-old-versions t)
   (setq auto-package-update-hide-results t)
   (auto-package-update-maybe)
+  (setq auto-package-update-prompt-before-update t)
   (setq auto-package-update-interval 4))
 
 (provide 'init-packages)
