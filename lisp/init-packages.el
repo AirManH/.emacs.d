103e55fb3ab6ce0c57c414e5fd24431642ad82a5
air <airmanhigh@gmail.com>
2020-10-18 19:27:20 +0800

    fix: problem of loading failed in MSYS2

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -18,8 +18,8 @@
 
 (defun my/packages-installed-p ()
   (cl-loop for pkg in my/packages
-	when (not (package-installed-p pkg)) do (return nil)
-	finally (return t)))
+	when (not (package-installed-p pkg)) do (cl-return nil)
+	finally (cl-return t)))
 
 (unless (my/packages-installed-p)
   (message "%s" "Refreshing package database...")
