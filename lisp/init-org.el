d25fce2ac8bc8b4f788701025a0cfb848a8b455e
air <airmanhigh@gmail.com>
2020-10-20 23:49:57 +0800

    refactor: add comment

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,4 +1,6 @@
-(provide 'init-org)
+;;; init-org.el --- Org mode -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
 
 
 (use-package org
@@ -7,25 +9,26 @@
   (setq org-src-fontify-natively t)
   (setq org-format-latex-options (plist-put org-format-latex-options :scale 2.0)))
 
-;; Live preview
+
 (defun air-org-imp-filter (buffer)
-  ;; org mode themes
-  (let ((theme_bigblow "#+SETUPFILE: https://fniessen.github.io/org-html-themes/setup/theme-bigblow.setup")
-        (theme_readtheorg "#+SETUPFILE: https://fniessen.github.io/org-html-themes/setup/theme-readtheorg.setup")
-	)
-    ;;
-    (princ
-     (with-current-buffer buffer
-       ;; (org-export-string-as (format "%s\n\n%s" theme_bigblow (buffer-string)) 'html)
-       (org-export-string-as (buffer-string) 'html)
-       )
-     (current-buffer)))
-  )
+  "A filter transfer current BUFFER to html."
+  (princ
+   (with-current-buffer buffer
+     ;; (org-export-string-as (format "%s\n\n%s" theme_bigblow (buffer-string)) 'html)
+     (org-export-string-as (buffer-string) 'html)
+     )
+   (current-buffer)))
+
 
 (defun air-org-preview ()
+  "Live preview current opened org file in default browser."
   (interactive)
   (unless (process-status "httpd")
     (httpd-start))
   (impatient-mode)
   (imp-set-user-filter 'air-org-imp-filter)
   (imp-visit-buffer))
+
+
+(provide 'init-org)
+;;; init-org.el ends here
