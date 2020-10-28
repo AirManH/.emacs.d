7d5843d96496a3670c01016b4a4c6161e80d6e65
air <airmanhigh@gmail.com>
2020-10-28 17:55:15 +0800

    feat: support fixing the punctuation width of pyim in dir-local vars
    To fix the width of punctuation while using pyim,
    set air-pyim-fix-punctuation-width to t,
    and set pyim-punctuation-translate-p properly.
    
    see also https://github.com/tumashu/pyim/issues/372

--- lisp/init-pyim.el
+++ lisp/init-pyim.el
@@ -7,20 +7,28 @@
 (global-set-key (kbd "C-\\") 'toggle-input-method)
 (setq default-input-method "pyim")
 
+(defvar air-pyim-fix-punctuation-width nil
+  "Whether the punctuation width should be fixed.")
+
 (use-package pyim
   :defer t
   :config
-  (use-package pyim-basedict
-    :config
-    (pyim-basedict-enable))
-  ;; my personal choice: xiaohe-shuangpin
-  (setq pyim-default-scheme 'xiaohe-shuangpin)
-  ;; show the first 5 candidate words
-  (setq pyim-page-length 5)
-  ;; use posframe to get better popup
-  (use-package posframe
-    :config
-    (setq pyim-page-tooltip 'popup))
+  (progn
+    ;; @see https://github.com/tumashu/pyim/issues/372
+    (when air-pyim-fix-punctuation-width
+      (setq pyim-local-variable-list
+            (remove 'pyim-punctuation-translate-p pyim-local-variable-list)))
+    (use-package pyim-basedict
+      :config
+      (pyim-basedict-enable))
+    ;; my personal choice: xiaohe-shuangpin
+    (setq pyim-default-scheme 'xiaohe-shuangpin)
+    ;; show the first 5 candidate words
+    (setq pyim-page-length 5)
+    ;; use posframe to get better popup
+    (use-package posframe
+      :config
+      (setq pyim-page-tooltip 'popup)))
   )
 
 
