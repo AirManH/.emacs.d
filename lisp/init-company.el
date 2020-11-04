e7af91655b78ccf114ad9ce5af4d0b2a6e2dbb92
air <airmanhigh@gmail.com>
2020-11-04 22:13:40 +0800

    ui: hide company, company-posframe from mode-line

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -5,12 +5,14 @@
 
 (use-package company
   :defer 1
+  :delight
   :config
   (progn
     (global-company-mode 1)
     (setq company-idle-delay 0.0
           company-minimum-prefix-length 1)
     (use-package company-posframe
+      :delight
       :config
       (progn
         (company-posframe-mode 1)))
