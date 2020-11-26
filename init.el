c9adb1ccc3d9729e3e4c7dab4e5c3588351c5a59
air <airmanhigh@gmail.com>
2020-11-27 01:30:36 +0800

    ui: use doom-themes instead of spacemacs-theme

--- init.el
+++ init.el
@@ -56,7 +56,7 @@
  '(custom-safe-themes
    '("f3ab34b145c3b2a0f3a570ddff8fabb92dafc7679ac19444c31058ac305275e1" default))
  '(package-selected-packages
-   '(highlight-indent-guides yaml-mode doom-modeline highlight-thing delight company-posframe texmathp posframe pyim cmake-mode cnfonts srefactor company-c-headers sr-speedbar flycheck smex simple-httpd company-anaconda anaconda-mode spacemacs-theme vmd-mode counsel rainbow-delimiters use-package format-all lua-mode hungry-delete company smartparens monokai-theme))
+   '(doom-themes highlight-indent-guides yaml-mode doom-modeline highlight-thing delight company-posframe texmathp posframe pyim cmake-mode cnfonts srefactor company-c-headers sr-speedbar flycheck smex simple-httpd company-anaconda anaconda-mode vmd-mode counsel rainbow-delimiters use-package format-all lua-mode hungry-delete company smartparens monokai-theme))
  '(safe-local-variable-values
    '((air-pyim-fix-punctuation-width . t)
      (pyim-punctuation-translate-p quote
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -30,11 +30,24 @@
   :config
   (smooth-scrolling-mode 1))
 
-;; see https://github.com/nashamri/spacemacs-theme/issues/42#issuecomment-236437989
-;; for more info
-(use-package spacemacs-common
-  :ensure spacemacs-theme
-  :config (load-theme 'spacemacs-dark t))
+(use-package doom-themes
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+  (doom-themes-treemacs-config)
+
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 
 ;; === GUI ===
 
