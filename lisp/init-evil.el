b55ee562a6a98ebac8ffc0e171ef4441024f25e9
air <airmanhigh@gmail.com>
2020-12-22 18:13:51 +0800

    fix&evil&org: bind TAB to `org-cycle'
    see also https://github.com/Somelauw/evil-org-mode/issues/54

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -20,14 +20,18 @@
 
 ;; https://github.com/Somelauw/evil-org-mode#installation-by-use-package
 (use-package evil-org
-  :after org
+  :hook (org-mode . evil-org-mode)
   :config
-  (add-hook 'org-mode-hook 'evil-org-mode)
-  (add-hook 'evil-org-mode-hook
-            (lambda ()
-              (evil-org-set-key-theme)))
-  (require 'evil-org-agenda)
-  (evil-org-agenda-set-keys))
+  (progn
+    (with-eval-after-load 'evil
+      (evil-define-key 'normal outline-mode-map (kbd "<tab>") #'org-cycle)
+      (evil-define-key 'normal outline-mode-map (kbd "TAB") #'org-cycle))
+    (add-hook 'evil-org-mode-hook
+              (lambda ()
+                (evil-org-set-key-theme)))
+    (require 'evil-org-agenda)
+    (evil-org-agenda-set-keys))
+  )
 
 
 (provide 'init-evil)
