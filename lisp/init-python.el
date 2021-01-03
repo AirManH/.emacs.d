53a1135d7e9475740fc358750e326d35c8cda4e6
air <airmanhigh@gmail.com>
2021-01-03 23:13:42 +0800

    feat: use `conda' to activate/deactivate conda virtual environment

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -16,5 +16,33 @@
 (use-package company-anaconda
   :hook (python-mode . air-python-set-company-backends-anaconda))
 
+(use-package conda
+  :after (anaconda-mode)
+  :config
+  (progn
+    ;; The location of your anaconda home will be guessed from a list of common
+    ;; possibilities, starting with `conda-anaconda-home''s default value (which
+    ;; will consult a ANACONDA_HOME envvar, if it exists).
+    ;;
+    ;; If none of these work for you, `conda-anaconda-home' must be set
+    ;; explicitly. Afterwards, run M-x `conda-env-activate' to switch between
+    ;; environments
+    (or (cl-loop for dir in (list conda-anaconda-home
+                                  "~/.anaconda"
+                                  "~/.miniconda"
+                                  "~/.miniconda3"
+                                  "~/anaconda3"
+                                  "~/miniconda3"
+                                  "~/opt/miniconda3"
+                                  "/usr/bin/anaconda3"
+                                  "/usr/local/anaconda3"
+                                  "/usr/local/miniconda3"
+                                  "/usr/local/Caskroom/miniconda/base")
+                 if (file-directory-p dir)
+                 return (setq conda-anaconda-home (expand-file-name dir)
+                              conda-env-home-directory (expand-file-name dir)))
+        (message "Cannot find Anaconda installation"))))
+
+
 (provide 'init-python)
 ;;; init-python.el ends here
