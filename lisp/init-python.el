14fb17e3f71838bee9da8e7e0da2b5325d7efacd
air <airmanhigh@gmail.com>
2021-01-03 16:10:47 +0800

    chore: reindent `lisp/init-python.el'

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -3,12 +3,12 @@
 
 (use-package anaconda-mode
   :hook ((python-mode . anaconda-mode)
-	 (python-mode . anaconda-eldoc-mode)))
+         (python-mode . anaconda-eldoc-mode)))
 
 (use-package company-anaconda
   :after (anaconda-mode company)
-  :init
-  (add-hook 'python-mode-hook
-	    (lambda ()
-	      (set (make-local-variable 'company-backends) '((company-anaconda company-dabbrev-code)
-							     company-dabbrev)))))
+  :config
+  (add-hook 'python-mode-hook (lambda ()
+                                (set (make-local-variable 'company-backends)
+                                     '((company-anaconda company-dabbrev-code)
+                                       company-dabbrev)))))
