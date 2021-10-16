7274b4929a45ccb2ba9736777cc6a9dbf42267e3
air <airmanhigh@gmail.com>
2021-10-17 01:07:58 +0800

    docker: use docker-tramp to access files in the docker container

--- lisp/init-docker.el
+++ lisp/init-docker.el
@@ -10,5 +10,15 @@
   (setq format-all-debug t))
 
 
+(use-package docker-tramp
+  :config
+  (progn
+    ;; Recognize the $PATH on remote
+    ;; See also https://www.gnu.org/software/emacs/manual/html_node/tramp/Remote-programs.html
+    (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+    )
+  )
+
+
 (provide 'init-docker)
 ;;; init-docker.el ends here
