9b07afd63ef13350bcba439d46b672d22161b178
air <airmanhigh@gmail.com>
2021-10-16 16:35:56 +0800

    key: use general.el to manage keybinds
    Two leader key is introduced:
    - `air-leader` ("SPC") is the global leader key.
    - `air-local-leader` ("SPC m") is the local leader key.
    
    Two keybinds definer is introduced:
    - `air-leader-def` to define a keybinds start with `air-leader`
    - `air-local-leader-def` to define a keybinds start with
      `air-local-leader`
    
    See also https://github.com/noctuid/general.el

--- lisp/init-key.el
+++ lisp/init-key.el
@@ -3,5 +3,20 @@
 ;;; Code:
 
 
+(use-package general
+  :config
+  (progn
+    (defvar air-leader "SPC"
+      "My leader key.")
+    (defvar air-local-leader "SPC m"
+      "My local leader key.")
+    (general-create-definer air-leader-def
+      :prefix air-leader)
+    (general-create-definer air-local-leader-def
+      :prefix air-local-leader)
+    )
+  )
+
+
 (provide 'init-key)
 ;;; init-key.el ends here
