b6999a4e1c4270fee7eafb927df8d382a5db3506
air <airmanhigh@gmail.com>
2021-12-16 11:42:59 +0800

    elpy: add several keybindings

--- lisp/init-python-elpy.el
+++ lisp/init-python-elpy.el
@@ -8,10 +8,44 @@
   :init
   (progn
     (advice-add 'python-mode :before 'elpy-enable)
-    (air-local-leader-def 'normal python-mode-map
-      "g d" 'elpy-goto-definition
-      "g b" 'xref-pop-marker-stack
-      )
+
+    (general-define-key
+     :prefix air-local-leader
+     :states 'normal
+     :keymaps 'python-mode-map
+     "" '(nil :wk "local leader key")
+     ;; format
+     "f" '(nil :wk "format")
+     ;; goto
+     "g" '(nil :wk "go to")
+     "g d" '(elpy-goto-definition :wk "definition")
+     "g b" '(xref-pop-marker-stack :wk "go back")
+     "g a" '(elpy-goto-assignment :wk "assignment")
+     ;; show
+     "s" '(nil :wk "show")
+     "s d" '(elpy-goto-definition-other-window :wk "definition")
+     "s c" '(elpy-occur-definitions :wk "any definitions")
+     "s r" '(xref-find-references :wk "reference")
+     "s c" '(elpy-doc :wk "doc")
+     ;; refactor
+     "r" '(nil :wk "refactor")
+     "r r" '(elpy-refactor-rename :wk "rename")
+     "r v" '(elpy-refactor-extract-variable :wk "move to new variable")
+     "r f" '(elpy-refactor-extract-function :wk "move to new function")
+     "r i" '(elpy-refactor-inline :wk "inline this variable")
+     ;; interpreter
+     "i" '(nil :wk "interpreter")
+     "i e" '(elpy-shell-send-region-or-buffer :wk "evaluate")
+     "i o" '(elpy-shell-switch-to-shell :wk "switch to shell")
+     )
+    (general-define-key
+     :prefix air-local-leader
+     :states 'normal
+     :keymaps 'inferior-python-mode-map
+     "" '(nil :wk "local leader key")
+     "i" '(nil :wk "interpreter")
+     "i o" '(elpy-shell-switch-to-buffer :wk "switch to buffer")
+     )
     )
   )
 
